987fb24965f963beda19f4438ed9c603c12935e9
JonathanPA12 <jonathan.pineda.aleman@est.una.ac.cr>
2022-09-03 16:15:13 -0600

    estructura_jugador

--- main.c
+++ main.c
@@ -1,27 +1,40 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#include <unistd.h>
+#include <stdbool.h>
 
 int matriz[4][4];
 int fila3 = 3, columna3 = 3; //posicion de la matriz en la que se encuentra el 3
 int valor = 19; //valor que se va a intercambiar con el 3
 void imprimirMatriz(); //funcion para imprimir la matriz
 void llenarMatriz(); //funcion para llenar la matriz
-void buscarValor(int); //funcion para buscar el valor en la matriz
+void llenarRamdon();
+void vaciarMatriz();
+
+struct jugador{
+    char nombre[20];
+    int movimientos;
+} typedef jugador;
+struct jugador jugadores;
 
 
 int main () {
-  
+    
+    printf("cual es su nombre: ");
+    scanf("%s", jugadores.nombre);
+    printf("Bienvenido jugador %s ", jugadores.nombre);
+
     char caracter; //variable para leer un caracter
     char enter; //variable para leer un enter
     llenarMatriz(); 
-    buscarValor(valor);
+    //llenarRamdon();
     matriz[3][3] = 0;//el 3 se pone en 0 para que no se imprima
     imprimirMatriz();
   
     do{
-         caracter = getchar(); //se lee un caracter de entrada por teclado
-         enter = getchar(); //se lee un enter de entrada por teclado
+        caracter = getchar(); //se lee un caracter de entrada por teclado
+        enter = getchar(); //se lee un enter de entrada por teclado
         printf("caracter: %c\n", caracter);
         switch (caracter) //para moverse entre la matriz
         {
@@ -31,7 +44,10 @@ int main () {
                 matriz[fila3-1][columna3] = 0; //se pone en 0 la posicion de la fila [2][3] 
                 imprimirMatriz();
                 fila3--; //se decrementa la fila 
-            }else{printf("No se puede mover hacia arriba\n");}
+                jugadores.movimientos++;
+            }else{printf("No se puede mover hacia arriba\n"); 
+            jugadores.movimientos++;}
+
             break;
         case 's' ://mover abajo
             if(fila3 < 3){ //si la fila no es la ultima
@@ -39,7 +55,11 @@ int main () {
                 matriz[fila3+1][columna3] = 0;
                 imprimirMatriz();
                 fila3++;
-            }else{printf("No se puede mover mas abajo\n");}
+                   jugadores.movimientos++;
+            }else{printf("No se puede mover mas abajo\n"); 
+              jugadores.movimientos++;
+              
+              }
             break;
          case 'a' ://mover a la izquierda
             if(columna3 > 0){
@@ -47,7 +67,8 @@ int main () {
                 matriz[fila3][columna3-1] = 0;
                 imprimirMatriz();
                 columna3--;
-            }else{ printf("No se puede mover a la izquierda\n");}
+                   jugadores.movimientos++;
+            }else{ printf("No se puede mover a la izquierda\n");    jugadores.movimientos++;}
             break;
         case 'd' ://mover a la derecha
             if(columna3 < 3){
@@ -55,9 +76,13 @@ int main () {
                 matriz[fila3][columna3+1] = 0;
                 imprimirMatriz();
                 columna3++;
-            }else{ printf("No se puede mover a la derecha\n");}
+                   jugadores.movimientos++;
+            }else{ printf("No se puede mover a la derecha\n");    jugadores.movimientos++;}
             break;
         case 'e':
+            printf("Gracias por jugar %s ", jugadores.nombre);
+            printf("Movimientos: %d", jugadores.movimientos);
+
             break;
 
         default:
@@ -65,14 +90,13 @@ int main () {
             break;
         }
 
-
-
-
-       
     }while(caracter != 'e');
+  
+    vaciarMatriz();
 
     return 0;
 }
+
 void imprimirMatriz(){
 
     for(int i = 0; i < 4; i++){
@@ -88,6 +112,7 @@ void imprimirMatriz(){
     }
 }
 void llenarMatriz(){
+
     int contador = 1;
     srand(time(0));
     for (int i = 0; i<4; i++)
@@ -101,18 +126,13 @@ void llenarMatriz(){
         printf("\n");
     }
 }
-void buscarValor(int valor){
-    int aux;
-    for(int i = 0; i < 4; i++){
-        for(int j = 0; j < 4; j++){
-            if(matriz[i][j] == valor){
-                aux = matriz[i][j];
-                matriz[i][j] = matriz[fila3][columna3];
-                matriz[fila3][columna3] = aux;
-              
-            }
+void vaciarMatriz(){
+    for (int i = 0; i<4; i++)
+    {
+        for (int j = 0; j<4; j++)
+        {
+           matriz[i][j] = 0;
         }
+        printf("\n");
     }
 }
-
-
